9218180e6fc8c70539260c0348360a4a9f759354
root <qoo23502004@yahoo.com.tw>
2017-03-10 11:16:18 +0800

    fix string split

--- upload.py
+++ upload.py
@@ -18,51 +18,40 @@ key ="GF1QZWAA0819JA8M"  # Thingspeak channel to update
 def thermometer():
     while True: 
 
-        #node1x=0
-        #node1y=0
 
-        #node2x=0
-        #node2y=0
-
-        #node3x=0
-        #node3y=0
-
-        #node4x=0
-        #node4y=0
 
         string=ser.readline()
-        #print(string)
         nodeNumber=string.split(" ")[0]
-        #X=string.split(" ")[1]
-        #Y=string.split(" ")[2]
+        X=string.split(" ")[1]
+        Y=string.split(" ")[2]
 
         if nodeNumber=="1":
-            node1x=string.split(" ")[1]
-            node1y=string.split(" ")[2]
+
+            node1x=X
+            node1y=Y
             params = urllib.urlencode({'field1': node1x, 'field2':node1y , 'key':key})
-            #print(nodeNumber)
-            #print(node1x)
-            #print(node1y)
+
         if nodeNumber=="2":
-            node2x=string.split(" ")[1]
-            node2y=string.split(" ")[2]
+
+            node2x=X
+            node2y=Y
             params = urllib.urlencode({'field3':node2x , 'field4':node2y , 'key':key})
         if nodeNumber=="3":
-            node3x=string.split(" ")[1]
-            node3y=string.split(" ")[2]
+
+            node3x=X
+            node3y=Y
             params = urllib.urlencode({'field5':node3x , 'field6':node3y , 'key':key})
         if nodeNumber=="4":
-            node4x=string.split(" ")[1]
-            node4y=string.split(" ")[2]
+
+            node4x=X
+            node4y=Y
             params = urllib.urlencode({'field7':node4x , 'field8':node4y , 'key':key})
-            #print(nodeNumber)
-            #print(node4x)
-            #print(node4y)
+
 
         
 
         
-        #params = urllib.urlencode({'field1': node1x, 'field2':node1y , 'field3':node2x , 'field4':node2y , 'field5':node3x , 'field6':node3y , 'field7':node4x , 'field8':node4y , 'key':key})
+        
         headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
         conn = httplib.HTTPConnection("140.127.194.107:3000")
         try:
